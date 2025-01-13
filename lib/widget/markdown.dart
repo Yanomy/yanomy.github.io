b0a5e458ac7f8e5d097c2e0968fa723322a13f4c
yan <yan_h@hotmail.com>
2025-01-13 21:15:13 +0800

    Updated with used inherited style

--- lib/widget/markdown.dart
+++ lib/widget/markdown.dart
@@ -7,7 +7,7 @@ List<Widget> buildNodes(BuildContext context, List<md.Node> nodes) {
   List<Widget> widgets = [];
   for (md.Node node in nodes) {
     if (node is md.Element) {
-      widgets.add(_parseElement(context, node));
+      widgets.add(_parseTopLevelNode(context, node));
     } else {
       throw Exception("Unable to handle node type: ${node.runtimeType}");
     }
@@ -15,35 +15,59 @@ List<Widget> buildNodes(BuildContext context, List<md.Node> nodes) {
   return widgets;
 }
 
-Widget _parseElement(BuildContext context, md.Element element) {
-  HtmlTag tag = HtmlTag.of(element.tag);
-  List<TextSpan> children = [];
-  if (element.children != null) {
-    for (md.Node child in element.children!) {
-      children.addAll(_parseNode(context, child, tag.style(context)));
-    }
-  }
-  return tag.decorate(context, children);
-}
-
-List<TextSpan> _parseNode(
-    BuildContext context, md.Node node, TextStyle parentStyle,
-    [bool escape = true]) {
+Widget _parseTopLevelNode(BuildContext context, md.Node node) {
   if (node is md.Element) {
-    return _parseElementInline(context, node, parentStyle);
+    HtmlTag tag = HtmlTag.of(node.tag);
+    return _parseElement(context, node, tag.style(context));
   } else if (node is md.Text) {
-    return [TextSpan(text: node.textContent, style: parentStyle)];
+    return Text.rich(_parseText(context, node));
   } else if (node is md.UnparsedContent) {
-    return [
-      TextSpan(text: node.textContent, style: style.unparsedContent(context))
-    ];
+    return Text.rich(_parseUnparsedContent(context, node));
   } else {
     throw Exception("Unable to handle node type: ${node.runtimeType}");
   }
 }
 
-List<TextSpan> _parseElementInline(
-    BuildContext context, md.Element element, TextStyle parentStyle) {
+Widget _parseElement(BuildContext context, md.Element element,
+    [TextStyle? parentStyle]) {
+  HtmlTag tag = HtmlTag.of(element.tag);
+  List<TextSpan> children = [];
+  if (element.children != null) {
+    for (md.Node child in element.children!) {
+      children
+          .addAll(_parseNode(context, child, tag.style(context, parentStyle)));
+    }
+  }
+  return tag.decorate(context, children);
+}
+
+List<TextSpan> _parseNode(BuildContext context, md.Node node,
+    [TextStyle? parentStyle]) {
+  if (node is md.Element) {
+    return _parseElementInline(context, node, parentStyle);
+  } else if (node is md.Text) {
+    return [_parseText(context, node, parentStyle)];
+  } else if (node is md.UnparsedContent) {
+    return [_parseUnparsedContent(context, node, parentStyle)];
+  } else {
+    throw Exception("Unable to handle node type: ${node.runtimeType}");
+  }
+}
+
+TextSpan _parseText(BuildContext context, md.Text node,
+    [TextStyle? parentStyle]) {
+  return TextSpan(text: node.textContent, style: parentStyle);
+}
+
+TextSpan _parseUnparsedContent(BuildContext context, md.UnparsedContent node,
+    [TextStyle? parentStyle]) {
+  return TextSpan(
+      text: node.textContent,
+      style: style.unparsedContent(context, parentStyle));
+}
+
+List<TextSpan> _parseElementInline(BuildContext context, md.Element element,
+    [TextStyle? parentStyle]) {
   List<TextSpan> children = [];
   HtmlTag tag = HtmlTag.of(element.tag);
   if (element.children != null) {
